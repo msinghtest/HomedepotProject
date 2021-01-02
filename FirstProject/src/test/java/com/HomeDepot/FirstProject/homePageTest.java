8e0255426aea4b7f7e88223b6bd6d64dc960db04
msinghtest <mousamisingh1@gmail.com>
2021-01-02 11:15:17 -0700

    Modified Test file

--- FirstProject/src/test/java/com/HomeDepot/FirstProject/homePageTest.java
+++ FirstProject/src/test/java/com/HomeDepot/FirstProject/homePageTest.java
@@ -1,12 +1,21 @@
 package com.HomeDepot.FirstProject;
 
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class homePageTest extends homePage {
 
 @Test	
 public void browserTest() {
+	SoftAssert sa = new SoftAssert();
 	initiateBrowser();
+	boolean result=true;
+
+	sa.assertTrue(result);
+	sa.assertAll();
+	 
 }
 	
 	
